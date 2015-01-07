82edb1f4fc7179866069c0e5545d47dd4bf6caa4
Arget31 <njeremy12@gmail.com>
2015-01-07 15:58:53 +0100

    test guerrier

---
+++ v1415v5/TestGuerrier.java
@@ -0,0 +1,35 @@
+
+
+import java.rmi.RemoteException;
+import java.util.Random;
+
+import controle.Console;
+import element.*;
+
+/**
+ * Test de la Console avec un Element qui s'ajoute a l'Arene (apres lancement Arene et IHM). A lancer en plusieurs exemplaires.
+ */
+public class TestGuerrier {
+
+	public static void main(String[] args) {
+		
+		try {
+			int port = 5099; // par defaut, 5099
+			if (args.length > 0) {
+				port = Integer.parseInt(args[0]);
+			}
+			
+			String ipArene = "localhost"; // par défaut, localhost
+			if (args.length > 1) { 
+				ipArene = args[1];
+			}
+
+			Random r = new Random();
+			Personnage bidule = new Personnage("GuerrierB21", 10, 10, 10, 1, 6);
+			new Console(bidule, r.nextInt(100), r.nextInt(100), port, ipArene);
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ v1415v5/element/Guerrier.java
@@ -0,0 +1,15 @@
+package element;
+
+public class Guerrier extends Personnage {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public Guerrier() {
+		super("GuerrierB21", 10, 10, 10, 1, 6);
+		
+	}
+
+}
